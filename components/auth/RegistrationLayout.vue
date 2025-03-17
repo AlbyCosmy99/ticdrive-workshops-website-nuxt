1d4572276b99c4345f65c47370254a6c8da5981f
Andrei Albu <dinamo1999@icloud.com>
2025-03-17 22:16:48 +0100

    feat: implement login page - in progress

---
+++ components/auth/RegistrationLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <div class="bg-yellow-300 h-[5rem]">steps and logo</div>
+    <div class="bg-red-300 w-full flex flex-col md:flex-row flex-1">
+      <!-- Left Section -->
+      <div class="bg-green-300 flex-1 flex justify-center items-center p-4">
+        <div
+          class="relative w-[90%] h-[70vw] max-w-[500px] max-h-[500px] rounded-xl overflow-hidden"
+        >
+          <NuxtImg
+            :src="mainImage"
+            class="absolute inset-0 w-full h-full object-cover"
+            alt="Centered Image"
+          />
+        </div>
+      </div>
+
+      <!-- Right Section -->
+      <div class="bg-blue-300 flex-1 flex items-center justify-center p-4">
+        <slot name="rightContent"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  mainImage: {
+    type: String,
+  },
+});
+</script>
--- pages/auth/Login.vue
+++ pages/auth/Login.vue
@@ -1,24 +1,15 @@
 <template>
-  <div class="min-h-screen flex flex-col">
-    <div class="bg-yellow-300 h-[5rem]">steps and logo</div>
-    <div class="bg-red-300 w-full flex flex-col md:flex-row flex-1">
-      <!-- Left Section -->
-      <div class="bg-green-300 flex-1 flex justify-center items-center p-4">
-        <div
-          class="relative w-[90%] h-[70vw] max-w-[500px] max-h-[500px] rounded-xl overflow-hidden"
-        >
-          <NuxtImg
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI2RLOBO8DYvk8aAUNEs6DJzCJzlgHT7HfAg&s"
-            class="absolute inset-0 w-full h-full object-cover"
-            alt="Centered Image"
-          />
-        </div>
+  <AuthRegistrationLayout
+    main-image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI2RLOBO8DYvk8aAUNEs6DJzCJzlgHT7HfAg&s"
+  >
+    <template #rightContent>
+      <div class="flex flex-col justify-center">
+        <NuxtImg
+          src="/images/ticDriveLogo.png"
+          class="w-52 h-52 object-cover"
+        />
+        <p class="font-medium text-4xl text-tic">Benvenuto!</p>
       </div>
-
-      <!-- Right Section -->
-      <div class="bg-blue-300 flex-1 flex items-center justify-center p-4">
-        content
-      </div>
-    </div>
-  </div>
+    </template>
+  </AuthRegistrationLayout>
 </template>
