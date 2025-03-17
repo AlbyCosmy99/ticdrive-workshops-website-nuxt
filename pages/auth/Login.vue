7c74a531df8450baf6f5660defef0773ca43cd48
Andrei Albu <dinamo1999@icloud.com>
2025-03-17 15:09:48 +0100

    feat: setup structure of login page

--- pages/auth/Login.vue
+++ pages/auth/Login.vue
@@ -1,10 +1,24 @@
 <template>
-    <div class="bg-red-300">
-        <div>
-            <p class="font-bold">img</p>
+  <div class="min-h-screen flex flex-col">
+    <div class="bg-yellow-300 h-[5rem]">steps and logo</div>
+    <div class="bg-red-300 w-full flex flex-col md:flex-row flex-1">
+      <!-- Left Section -->
+      <div class="bg-green-300 flex-1 flex justify-center items-center p-4">
+        <div
+          class="relative w-[90%] h-[70vw] max-w-[500px] max-h-[500px] rounded-xl overflow-hidden"
+        >
+          <NuxtImg
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI2RLOBO8DYvk8aAUNEs6DJzCJzlgHT7HfAg&s"
+            class="absolute inset-0 w-full h-full object-cover"
+            alt="Centered Image"
+          />
         </div>
-        <div>
-            content
-        </div>
+      </div>
+
+      <!-- Right Section -->
+      <div class="bg-blue-300 flex-1 flex items-center justify-center p-4">
+        content
+      </div>
     </div>
+  </div>
 </template>
