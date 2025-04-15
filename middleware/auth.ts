7a299ab07549dc3d53382f0fb990d6d13b4cd9d2
Andrei Albu <dinamo1999@icloud.com>
2025-04-16 01:10:03 +0200

    feat: improve auth flow

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,12 +1,17 @@
-export default defineNuxtRouteMiddleware(async (to, from) => {
+export default defineNuxtRouteMiddleware(to => {
   if (import.meta.server) return;
 
   const token = localStorage.getItem('token');
 
-  if (!token) {
+  if (to.path === '/') {
+    return token ? navigateTo('/dashboard') : navigateTo('/auth/login');
+  }
+
+  if (!token && to.path !== '/auth/login') {
     return navigateTo('/auth/login');
   }
 
-  // TODO: API call to verify token validity
-  return navigateTo('/dashboard');
+  if (token && to.path === '/auth/login') {
+    return navigateTo('/dashboard');
+  }
 });
