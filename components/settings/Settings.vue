e0b8571ac76a73d29b96830d77569177a0235769
MaestroTheSimoni <96301217+MaestroTheSimoni@users.noreply.github.com>
2025-05-15 16:28:44 +0200

    fix;delete modifica dettagli e delete account

--- components/settings/Settings.vue
+++ components/settings/Settings.vue
@@ -5,15 +5,8 @@
         <h2 class="text-2xl font-semibold mb-4">Profilo Gestore Officina</h2>
         <div class="flex items-center gap-4 justify-between">
           <div class="flex items-center gap-4">
-            <div
-              v-if="authStore.user?.images.length"
-              class="rounded-full overflow-hidden h-[94px] w-[94px]"
-            >
-              <NuxtImg
-                :src="authStore.user?.images[0].url"
-                alt="Profile Image"
-                class="object-cover w-full h-full"
-              />
+            <div v-if="authStore.user?.images.length" class="rounded-full overflow-hidden h-[94px] w-[94px]">
+              <NuxtImg :src="authStore.user?.images[0].url" alt="Profile Image" class="object-cover w-full h-full" />
             </div>
             <div>
               <h3 class="text-lg font-semibold">{{ authStore.user?.name }}</h3>
@@ -22,12 +15,8 @@
               </p>
             </div>
           </div>
-          <TicDrivebutton
-            :label="isEditing ? (isSaving ? 'Salvando...' : 'Salva') : 'Modifica'"
-            @click="toggleEdit"
-            :disabled="isSaving"
-            custom-class="text-sm font-bold rounded-lg pl-2.5 pr-2.5 pt-2.5 pb-2.5"
-          />
+          <TicDrivebutton :label="isEditing ? (isSaving ? 'Salvando...' : 'Salva') : 'Modifica'" @click="toggleEdit"
+            :disabled="isSaving" custom-class="text-sm font-bold rounded-lg pl-2.5 pr-2.5 pt-2.5 pb-2.5" />
         </div>
       </div>
     </div>
@@ -37,113 +26,65 @@
         <h2 class="text-xl font-semibold mb-6">Impostazioni</h2>
 
         <div class="mb-6 border-b pb-6">
-          <label class="block text-base font-normal text-gray-700 mb-1"
-            >Nome Cognome</label
-          >
+          <label class="block text-base font-normal text-gray-700 mb-1">Nome Cognome</label>
           <div v-if="!isEditing" class="text-sm font-normal text-tic">
             {{ authStore.user?.name }}
           </div>
-          <TicDriveInput
-            v-else
-            v-model="userData.name"
-            placeholder="Inserisci il tuo nome completo"
-            size="small"
-          />
+          <TicDriveInput v-else v-model="userData.name" placeholder="Inserisci il tuo nome completo" size="small" />
         </div>
 
         <div class="mb-6 border-b pb-6">
-          <label class="block text-base font-normal text-gray-700 mb-1"
-            >Email</label
-          >
+          <label class="block text-base font-normal text-gray-700 mb-1">Email</label>
           <div v-if="!isEditing" class="text-sm font-normal text-tic">
             {{ authStore.user?.email }}
           </div>
-          <TicDriveInput
-            v-else
-            v-model="userData.email"
-            placeholder="Inserisci la tua email"
-            type="email"
-            size="small"
-          />
+          <TicDriveInput v-else v-model="userData.email" placeholder="Inserisci la tua email" type="email"
+            size="small" />
         </div>
 
         <div class="mb-6 border-b pb-6">
-          <label class="block text-base font-normal text-gray-700 mb-1"
-            >Numero di telefono</label
-          >
+          <label class="block text-base font-normal text-gray-700 mb-1">Numero di telefono</label>
           <div v-if="!isEditing" class="text-sm font-normal text-tic">
             {{
               authStore.user?.phoneNumber ||
               'Numero di telefono non disponibile'
             }}
           </div>
-          <TicDriveInput
-            v-else
-            v-model="userData.phoneNumber"
-            placeholder="Inserisci il tuo numero di telefono"
-            type="tel"
-            size="small"
-          />
+          <TicDriveInput v-else v-model="userData.phoneNumber" placeholder="Inserisci il tuo numero di telefono"
+            type="tel" size="small" />
         </div>
 
         <div class="mb-6 border-b pb-6">
-          <label class="block text-base font-normal text-gray-700 mb-1"
-            >Indirizzo</label
-          >
+          <label class="block text-base font-normal text-gray-700 mb-1">Indirizzo</label>
           <div v-if="!isEditing" class="text-sm font-normal text-tic">
             {{ authStore.user?.address || 'Indirizzo non disponibile' }}
           </div>
-          <TicDriveInput
-            v-else
-            v-model="userData.address"
-            placeholder="Inserisci il tuo indirizzo"
-            size="small"
-          />
+          <TicDriveInput v-else v-model="userData.address" placeholder="Inserisci il tuo indirizzo" size="small" />
         </div>
 
         <div class="mb-6 border-b pb-6">
-          <button
-            @click="changePassword"
-            class="text-drive text-base font-normal hover:text-green-700"
-          >
+          <button @click="changePassword" class="text-drive text-base font-normal hover:text-green-700">
             Cambio Password
           </button>
         </div>
 
-        <div class="mb-6 border-b pb-6">
-          <button
-            @click="modifyWorkshopDetails"
-            class="text-drive text-base font-normal hover:text-green-700"
-          >
-            Modifica dettagli Officina
-          </button>
-        </div>
+
 
         <div class="mb-6 border-b pb-6">
-          <button
-            @click="authStore.logout()"
-            class="text-drive text-base font-normal hover:text-green-700"
-          >
+          <button @click="authStore.logout()" class="text-red-500 text-base font-normal hover:text-red-700">
             Logout
           </button>
         </div>
 
-        <div class="pb-6">
-          <button
-            @click="deleteAccount"
-            class="text-[16] text-red-500 font-poppins hover:text-red-600"
-          >
-            Elimina account
-          </button>
-        </div>
+
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue';
-import {useRouter} from 'vue-router';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 import useAuthStore from '~/store/auth';
 import TicDrivebutton from '@/components/ui/buttons/TicDrivebutton.vue';
 import TicDriveInput from '@/components/ui/inputs/TicDriveInput.vue';
@@ -200,7 +141,7 @@ const toggleEdit = async () => {
       isSaving.value = true;
       await emit('updateProfile', userData.value);
       isEditing.value = false;
-      
+
       // Refresh the display data from the store after save
       userData.value = {
         name: authStore.user?.name || '',
