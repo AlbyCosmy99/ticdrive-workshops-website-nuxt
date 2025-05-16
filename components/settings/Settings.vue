22420b55c5d7f0c87c733fa318b1527ff06225cc
albycosmy99 <dinamo1999@icloud.com>
2025-05-17 00:11:14 +0200

    improve settings

--- components/settings/Settings.vue
+++ components/settings/Settings.vue
@@ -5,8 +5,15 @@
         <h2 class="text-2xl font-semibold mb-4">Profilo Gestore Officina</h2>
         <div class="flex items-center gap-4 justify-between">
           <div class="flex items-center gap-4">
-            <div v-if="authStore.user?.images.length" class="rounded-full overflow-hidden h-[94px] w-[94px]">
-              <NuxtImg :src="authStore.user?.images[0].url" alt="Profile Image" class="object-cover w-full h-full" />
+            <div
+              v-if="authStore.user?.images.length"
+              class="rounded-full overflow-hidden h-[94px] w-[94px]"
+            >
+              <NuxtImg
+                :src="authStore.user?.images[0].url"
+                alt="Profile Image"
+                class="object-cover w-full h-full"
+              />
             </div>
             <div>
               <h3 class="text-lg font-semibold">{{ authStore.user?.name }}</h3>
@@ -16,11 +23,20 @@
             </div>
           </div>
           <div class="flex gap-2">
-            <TicDrivebutton v-if="isEditing" label="Annulla" @click="cancelEdit"
-              custom-class="text-sm font-bold rounded-lg pl-2.5 pr-2.5 pt-2.5 pb-2.5 w-[100px] bg-red-500 hover:bg-red-600 text-white" />
-            <TicDrivebutton :label="isEditing ? (loading ? 'Salvando...' : 'Salva') : 'Modifica'
-              " @click="onEditProfile" :disabled="loading"
-              custom-class="text-sm font-bold rounded-lg pl-2.5 pr-2.5 pt-2.5 pb-2.5 w-[100px]" />
+            <TicDrivebutton
+              :label="
+                isEditing ? (loading ? 'Salvando...' : 'Salva') : 'Modifica'
+              "
+              @click="onEditProfile"
+              :disabled="loading"
+              custom-class="text-sm font-bold rounded-lg pl-2.5 pr-2.5 pt-2.5 pb-2.5 w-[100px]"
+            />
+            <TicDrivebutton
+              v-if="isEditing"
+              label="Annulla"
+              @click="cancelEdit"
+              custom-class="text-sm font-bold rounded-lg pl-2.5 pr-2.5 pt-2.5 pb-2.5 w-[100px] bg-red-500 hover:bg-red-600 text-white"
+            />
           </div>
         </div>
       </div>
@@ -31,27 +47,42 @@
         <h2 class="text-xl font-semibold mb-6">Impostazioni</h2>
 
         <div class="mb-6 border-b pb-6">
-          <label class="block text-base font-normal text-gray-700 mb-1">Nome Cognome</label>
+          <label class="block text-base font-normal text-gray-700 mb-1"
+            >Nome Cognome</label
+          >
           <div v-if="!isEditing" class="text-sm font-normal text-tic">
             {{ authStore.user?.name }}
           </div>
           <div v-else class="w-[400px]">
-            <TicDriveInput v-model="userData.name" placeholder="Inserisci il tuo nome completo" size="small" />
+            <TicDriveInput
+              v-model="userData.name"
+              placeholder="Inserisci il tuo nome completo"
+              size="small"
+            />
           </div>
         </div>
 
         <div class="mb-6 border-b pb-6">
-          <label class="block text-base font-normal text-gray-700 mb-1">Email</label>
+          <label class="block text-base font-normal text-gray-700 mb-1"
+            >Email</label
+          >
           <div v-if="!isEditing" class="text-sm font-normal text-tic">
             {{ authStore.user?.email }}
           </div>
           <div v-else class="w-[400px]">
-            <TicDriveInput v-model="userData.email" placeholder="Inserisci la tua email" type="email" size="small" />
+            <TicDriveInput
+              v-model="userData.email"
+              placeholder="Inserisci la tua email"
+              type="email"
+              size="small"
+            />
           </div>
         </div>
 
         <div class="mb-6 border-b pb-6">
-          <label class="block text-base font-normal text-gray-700 mb-1">Numero di telefono</label>
+          <label class="block text-base font-normal text-gray-700 mb-1"
+            >Numero di telefono</label
+          >
           <div v-if="!isEditing" class="text-sm font-normal text-tic">
             {{
               authStore.user?.phoneNumber ||
@@ -59,32 +90,60 @@
             }}
           </div>
           <div v-else class="w-[400px]">
-            <TicDriveInput v-model="userData.phoneNumber" placeholder="Inserisci il tuo numero di telefono" type="tel"
-              size="small" />
+            <TicDriveInput
+              v-model="userData.phoneNumber"
+              placeholder="Inserisci il tuo numero di telefono"
+              type="tel"
+              size="small"
+            />
           </div>
         </div>
 
         <div class="mb-6 border-b pb-6">
-          <label class="block text-base font-normal text-gray-700 mb-1">Indirizzo</label>
+          <label class="block text-base font-normal text-gray-700 mb-1"
+            >Indirizzo</label
+          >
           <div v-if="!isEditing" class="text-sm font-normal text-tic">
             {{ authStore.user?.address || 'Indirizzo non disponibile' }}
           </div>
           <div v-else class="flex flex-col w-[400px] gap-2">
-            <TicDriveInput v-model="userData.address.streetAddress" placeholder="Via e Numero Civico" size="small" />
-            <TicDriveInput v-model="userData.address.city" placeholder="Città" size="small" />
-            <TicDriveInput v-model="userData.address.province" placeholder="Provincia" size="small" />
-            <TicDriveInput v-model="userData.address.postalCode" placeholder="CAP" size="small" />
+            <TicDriveInput
+              v-model="userData.address.streetAddress"
+              placeholder="Via e Numero Civico"
+              size="small"
+            />
+            <TicDriveInput
+              v-model="userData.address.city"
+              placeholder="Città"
+              size="small"
+            />
+            <TicDriveInput
+              v-model="userData.address.province"
+              placeholder="Provincia"
+              size="small"
+            />
+            <TicDriveInput
+              v-model="userData.address.postalCode"
+              placeholder="CAP"
+              size="small"
+            />
           </div>
         </div>
 
         <div class="mb-6 border-b pb-6">
-          <button @click="changePassword" class="text-drive text-base font-normal hover:text-green-700">
+          <button
+            @click="changePassword"
+            class="text-drive text-base font-normal hover:text-green-700"
+          >
             Cambio Password
           </button>
         </div>
 
         <div class="mb-6 border-b pb-6">
-          <button @click="authStore.logout()" class="text-red-500 text-base font-normal hover:text-red-700">
+          <button
+            @click="authStore.logout()"
+            class="text-red-500 text-base font-normal hover:text-red-700"
+          >
             Logout
           </button>
         </div>
@@ -94,7 +153,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import {ref} from 'vue';
 import useAuthStore from '~/store/auth';
 import TicDrivebutton from '@/components/ui/buttons/TicDrivebutton.vue';
 import TicDriveInput from '@/components/ui/inputs/TicDriveInput.vue';
@@ -111,17 +170,14 @@ const userData = ref({
     streetAddress: '',
     city: '',
     province: '',
-    postalCode: ''
-  }
+    postalCode: '',
+  },
 });
-
 
 if (authStore.user?.address) {
   try {
-
     const addressParts = authStore.user.address.split(',');
     userData.value.address.streetAddress = addressParts[0]?.trim() || '';
-
 
     if (addressParts[1]) {
       const cityParts = addressParts[1].trim().split(' ');
@@ -130,7 +186,6 @@ if (authStore.user?.address) {
         userData.value.address.city = cityParts.slice(1).join(' ') || '';
       }
     }
-
 
     if (addressParts[2]) {
       userData.value.address.province = addressParts[2].trim() || '';
@@ -164,34 +219,28 @@ const modifyProfile = () => {
 };
 
 const cancelEdit = () => {
-
   userData.value.name = '';
   userData.value.email = '';
   userData.value.phoneNumber = '';
-
 
   userData.value.address = {
     streetAddress: '',
     city: '',
     province: '',
-    postalCode: ''
+    postalCode: '',
   };
-
-
+  isEditing.value = false;
 };
 
 const onEditProfile = () => {
   if (isEditing.value) {
     // Format address string like in registration(commento lasciato per essere sicuri che sia formattato/messo insieme giusto)
-    const { streetAddress, postalCode, city, province } = userData.value.address;
+    const {streetAddress, postalCode, city, province} = userData.value.address;
     const formattedAddress = `${streetAddress}, ${postalCode} ${city}, ${province}, Italy`;
 
-
     console.log('Formatted address:', formattedAddress);
-
-
   }
 
   isEditing.value = !isEditing.value;
-}
+};
 </script>
