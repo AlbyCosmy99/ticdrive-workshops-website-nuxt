10acd7073164ee363dfa984acc5932ec7bd722f7
albycosmy99 <dinamo1999@icloud.com>
2025-05-24 22:04:15 +0200

    feat: add sitemap.xml configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,5 +24,43 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     'pinia-plugin-persistedstate/nuxt',
     '@nuxtjs/tailwindcss',
+    [
+      '@nuxtjs/sitemap',
+      {
+        sitemap: {
+          siteUrl: 'https://www.ticdrive.it',
+          trailingSlash: false,
+          autoLastmod: true,
+        },
+      },
+    ],
   ],
+
+  app: {
+    head: {
+      title: 'TicDrive – Prenota servizi auto',
+      meta: [
+        {
+          name: 'description',
+          content:
+            'TicDrive è l’app che ti permette di prenotare servizi auto facilmente, confrontando officine e prezzi vicino a te.',
+        },
+        {
+          name: 'keywords',
+          content:
+            'ticdrive, officine, auto, prenotazione, meccanico, revisione, cambio olio',
+        },
+        {property: 'og:title', content: 'TicDrive – Prenota servizi auto'},
+        {
+          property: 'og:description',
+          content:
+            'Prenota servizi per la tua auto con TicDrive: trova officine, confronta prezzi e leggi recensioni.',
+        },
+        {property: 'og:type', content: 'website'},
+        {property: 'og:url', content: 'https://www.ticdrive.it'},
+        {property: 'og:image', content: '/images/ticDriveLogo.png'},
+      ],
+      link: [{rel: 'icon', type: 'image/png', href: '/favicon.png'}],
+    },
+  },
 });
