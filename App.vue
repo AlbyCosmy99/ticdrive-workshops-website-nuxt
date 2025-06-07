1912a1477147354cecfe07fa24a8d1f832036340
albycosmy99 <dinamo1999@icloud.com>
2025-06-07 23:44:11 +0200

    feat: add footer

--- App.vue
+++ App.vue
@@ -1,5 +1,14 @@
 <template>
   <NuxtLayout>
     <NuxtPage />
+
+    <footer style="margin-top: 3rem; padding: 2rem; background-color: #f5f5f5; text-align: center;">
+      <p>
+        © {{ new Date().getFullYear() }} TicDrive ·
+        <a class="underline" href="https://www.ticdrive.it/privacy-policy/app" target="_blank" rel="noopener noreferrer">
+          Norme sulla privacy
+        </a>
+      </p>
+    </footer>
   </NuxtLayout>
 </template>
