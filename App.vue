3177c4933ab2b06f28306d00363e1fc46c4fc2f5
albycosmy99 <dinamo1999@icloud.com>
2025-06-08 00:12:11 +0200

    fix

--- App.vue
+++ App.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import Footer from './components/Footer.vue';
 
+definePageMeta({
+  title: 'TicDrive - Prenota servizi auto online',
+  description: 'Scopri TicDrive: l’app che ti aiuta a trovare, confrontare e prenotare servizi per la tua auto. Facile, veloce e affidabile.',
+});
+
 </script>
 
 <template>
