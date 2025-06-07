9bec137ef700d3f9add43790740f2ead14fda0a8
albycosmy99 <dinamo1999@icloud.com>
2025-06-07 23:52:21 +0200

    feat: add footer component

---
+++ components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer
+    style="
+      margin-top: 3rem;
+      padding: 2rem;
+      background-color: #f5f5f5;
+      text-align: center;
+    "
+  >
+    <p>
+      © {{ new Date().getFullYear() }} TicDrive ·
+      <a
+        class="underline"
+        href="https://www.ticdrive.it/privacy-policy/app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Norme sulla privacy
+      </a>
+    </p>
+  </footer>
+</template>
